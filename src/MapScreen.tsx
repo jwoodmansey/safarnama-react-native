04df712661451565c021142a0d67bbf18eb38985
Jack Woodmansey <jwoodmansey@live.co.uk>
2020-11-08 15:40:05 +0000

    adding icon types

--- src/MapScreen.tsx
+++ src/MapScreen.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
-
+import MapView, { Marker } from 'react-native-maps';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const MapScreen: React.FC = () => {
   return (
     <View style={styles.map}>
       <MapView
+        showsUserLocation
+        showsMyLocationButton
+        showsScale
         style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      // initialRegion={{
+      //   latitude: -2,
+      //   longitude: 50.6,
+      //   latitudeDelta: 0.0922,
+      //   longitudeDelta: 0.0421,
+      // }}
+      ><Marker title="Test" pinColor={"#202020"} coordinate={{ latitude: 54, longitude: -2.81 }} >
+          <Icon size={20} name="camera"></Icon>
+        </Marker>
+      </MapView>
 
     </View>
   )
