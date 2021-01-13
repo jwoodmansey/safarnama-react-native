26942fabca22f737508bb7b8989396b1b60d27c5
Jack Woodmansey <jack.woodmansey@vibepay.com>
2021-01-13 20:57:01 +0000

    adding instructions to QR code scanner

--- src/features/qr-code/screens/ScanQRCodeScreen.tsx
+++ src/features/qr-code/screens/ScanQRCodeScreen.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Title } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Colors, Title } from "react-native-paper";
 import QRCodeScanner from "react-native-qrcode-scanner";
 
 const ScanQRCodeScreen: React.FC = () => {
@@ -7,13 +8,32 @@ const ScanQRCodeScreen: React.FC = () => {
   return (
     <QRCodeScanner
       onRead={onSuccess}
+      showMarker
       topContent={
-        <Title style={{ flex: 1 }}>
-          Point your camera at a Safarnama QR code!
-        </Title>
+        <View style={styles.instructionsContainer}>
+          <Title style={styles.insturctionsText}>
+            Point your camera at a Safarnama QR code
+          </Title>
+        </View>
       }
     />
   );
 };
+
+const styles = StyleSheet.create({
+  instructionsContainer: {
+    zIndex: 10,
+    margin: 40,
+    top: 40,
+    backgroundColor: Colors.black,
+    padding: 16,
+    borderRadius: 8,
+  },
+  insturctionsText: {
+    textAlign: "center",
+    color: Colors.white,
+    justifyContent: "center",
+  },
+});
 
 export default ScanQRCodeScreen;
