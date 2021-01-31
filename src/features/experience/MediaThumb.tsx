a82ced2d7b961665dc2815c5f44ec65d7856789d
Jack Woodmansey <jack.woodmansey@vibepay.com>
2021-01-31 21:38:06 +0000

    WIP: trying to get video to work with native controls

--- src/features/experience/MediaThumb.tsx
+++ src/features/experience/MediaThumb.tsx
@@ -1,11 +1,21 @@
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  Alert,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import FastImage from "react-native-fast-image";
 import { Caption, Colors } from "react-native-paper";
 import Pdf from "react-native-pdf";
 import HTML from "react-native-render-html";
+import Video, { OnPlaybackRateData } from "react-native-video";
+// import { Video as ExpoVideo } from "expo-av";
 import { getPath } from "../../store/mediaService";
 import { MediaDocument } from "../../types/common/media";
 import { getMediaType, MediaType } from "../../types/media";
@@ -17,6 +27,9 @@ type Props = {
 
 const MediaThumb: React.FC<Props> = ({ media }) => {
   const nav = useNavigation<StackNavigationProp<MapNaviationProp>>();
+  const [isPaused, setIsPaused] = useState<boolean>(true);
+  const [isBuffering, setIsBuffering] = useState(false);
+  const ref = useRef();
   switch (getMediaType(media.mimetype)) {
     case MediaType.Image: {
       const onImagePress = () => {
@@ -31,6 +44,78 @@ const MediaThumb: React.FC<Props> = ({ media }) => {
             }}
           />
         </TouchableOpacity>
+      );
+    }
+    case MediaType.Video: {
+      const onPlaybackRateChange = (data: OnPlaybackRateData) => {
+        // Alert.alert("playback", JSON.stringify(data));
+        // if (data.playbackRate === 0) {
+        //   setIsPaused(false);
+        // } else {
+        //   setIsPaused(false);
+        // }
+      };
+      const playVideo = () => {
+        if (Platform.OS === "ios") {
+          setIsPaused(false);
+        }
+      };
+      const onBuffer = (meta: OnBufferData) => {
+        // Are we starting to buffer?
+        // if (meta.isBuffering && !isBuffering) {
+        //   animateBuffer();
+        // }
+
+        // // Did we just finished buffering?
+        // if (!meta.isBuffering && isBuffering) {
+        //   if (loopedBufferAnimation) {
+        //     loopedBufferAnimation.stop();
+        //   }
+        // }
+
+        setIsBuffering(meta.isBuffering);
+      };
+
+      return (
+        <View>
+          <ExpoVideo
+            // onPlaybackRateChange={onPlaybackRateChange}
+            // onExternalPlaybackChange={() => {
+            //   // Alert.alert("On External");
+            // }}
+            // onPlaybackResume={() => {
+            //   // Alert.alert("Resume");
+            //   // setIsPaused(false);
+            // }}
+            // onProgress={() => {
+            //   // Alert.alert("Test");
+            // }}
+            // controls
+            // paused={isPaused}
+            // rate={0}
+            // allowsExternalPlayback
+            // onBuffer={onBuffer}
+            // resizeMode="contain"
+            useNativeControls
+            ref={ref}
+            usePoster={false}
+            shouldPlay={!isPaused}
+            style={{ height: 150, width: "100%" }}
+            source={{ uri: getPath(media) }}
+          />
+          <View>
+            <Pressable onPress={playVideo}>
+              <Text>Play!!!</Text>
+            </Pressable>
+          </View>
+          {/* {isPaused && !isBuffering && (
+            <View>
+              <Pressable onPress={playVideo}>
+                <Text>Play</Text>
+              </Pressable>
+            </View>
+          )} */}
+        </View>
       );
     }
     case MediaType.Text: {
