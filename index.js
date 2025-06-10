e9155ce229fc560c82eb125da7a10a7b64e2386d
Jack Woodmansey <jackwoodmansey@gmail.com>
2025-06-10 20:58:32 +0100

    Swap to notifee push lib

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ import { AppRegistry } from "react-native";
 import BackgroundFetch from "react-native-background-fetch";
 import BackgroundGeolocation from "react-native-background-geolocation";
 import "react-native-gesture-handler";
-// import PushNotification from "react-native-push-notification";
 import { enableScreens } from "react-native-screens";
 // import Constants from "expo-constants";
 import App from "./App";
 import { name as appName } from "./app.json";
-import { navigate } from "./src/nav/NavigationRef";
-import { sendPlacePush } from "./src/utils/pushNotifications";
+import {
+  listenForPushPressEvents,
+  sendPlacePush,
+} from "./src/utils/pushNotifications";
 
 // Before rendering any navigation stack
 enableScreens();
@@ -73,17 +74,7 @@ const HeadlessTask = async (event) => {
 //   });
 // };
 
-// PushNotification.configure({
-//   requestPermissions: false,
-//   onNotification: (e) => {
-//     if (e.data.placeId) {
-//       navigate("ViewPlaceScreen", {
-//         placeId: e.data.placeId,
-//         name: e.data.name,
-//       });
-//     }
-//   },
-// });
+listenForPushPressEvents();
 
 AppRegistry.registerComponent(appName, () => App);
 
